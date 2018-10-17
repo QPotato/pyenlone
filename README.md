1a0dbaf5004cd8dd3d735a87f672e4b9bd46bbf5
fede <federicobadaloni@hotmail.com>
2018-10-17 17:49:04 -0300

    docstrings and minor fixes

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# pyenlone
+## pyenlone
 
-Python implementation for enl.one API
+Python implementation of enl.one APIs:
+- https://v.enl.one
+- https://tasks.enl.one (Soon)
+- https://status.enl.one (Not very soon, but soon)
+For more info: https://wiki.enl.one/doku.php?id=start
+
+All methods returns Munches. That only means you access them both as:
+- `some_result.some_property`
+- `some_result['some_property']`
--- pyenlone/_proxy.py
+++ pyenlone/_proxy.py
@@ -1,4 +1,4 @@
-""" Proxies do the actual API calls. """
+""" Proxies do the actual API requests. """
 from abc import ABC, abstractmethod
 
 import requests
@@ -8,16 +8,27 @@ from munch import munchify
 from .enloneexception import EnlOneException
 
 class Proxy(ABC):
+    """
+    Proxy interface.
+    """
     @abstractmethod
     def get(self, endpoint, params): pass
+    @abstractmethod
+    def post(self, endpoint, json): pass
 
 class KeyProxy(Proxy):
+    """
+    Proxy implementation for authentication using API keys.
+    """
     def __init__(self, base_url, apikey, cache=0):
         self._apikey = apikey
         self._base_url = base_url
         if cache > 0:
             requests_cache.install_cache('apikey_cache', backend='sqlite', expire_after=cache)
     def get(self, endpoint, params={}):
+        """
+        Do a get request adding the apikey as a parameter.
+        """
         url = self._base_url + endpoint
         params["apikey"] = self._apikey
         try:
@@ -29,6 +40,9 @@ class KeyProxy(Proxy):
         else:
             raise EnlOneException("enl.one API call error.")
     def post(self, endpoint, json):
+        """
+        Do a post request adding the apikey as a parameter.
+        """
         url = self._base_url + endpoint
         try:
             response = requests.post(url, params={"apikey" : self._apikey}, json=json)
@@ -40,12 +54,18 @@ class KeyProxy(Proxy):
             raise EnlOneException("enl.one API call error.")
 
 class TokenProxy(Proxy):
+    """
+    Proxy implementation for authentication using OAuth tokens.
+    """
     def __init__(self, base_url, token, cache=0):
         self._token = token
         self._base_url = base_url + "/oauth"
         if cache > 0:
             requests_cache.install_cache('token_cache', backend='sqlite', expire_after=cache)
     def get(self, endpoint, params={}):
+        """
+        Do a get request adding the Authorization header.
+        """
         url = self._base_url + endpoint
         headers = {'Authorization':'Bearer ' + self._token}
         try:
@@ -57,6 +77,9 @@ class TokenProxy(Proxy):
         else:
             raise EnlOneException("enl.one API call error.")
     def post(self, endpoint, json):
+        """
+        Do a get request adding the Authorization header.
+        """
         url = self._base_url + endpoint
         headers = {'Authorization':'Bearer ' + self._token}
         try:
@@ -69,7 +92,13 @@ class TokenProxy(Proxy):
             raise EnlOneException("enl.one API call error.")
 
 class OpenProxy:
-    def get(endpoint):
+    """
+    Proxy implementation for the open API.
+    """
+    def get(self, endpoint):
+        """
+        Do a get request with no auth.
+        """
         url = "https://v.enl.one/OpenApi" + endpoint
         try:
             response = requests.get(url)
