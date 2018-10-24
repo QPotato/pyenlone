ad74e35790b1c3afda01175524ffdd58ce196679
fede <federicobadaloni@hotmail.com>
2018-10-23 21:03:35 -0300

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyenlone",
-    version="0.1.2",
+    version="0.1.3",
     author="QuanticPotato",
     description="A python implementation of enl.one API",
     long_description=long_description,
